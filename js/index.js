9727463a464ae6689b4fd0b9f3657e81ae733559
khushboojoshi3 <64426164+khushboojoshi3@users.noreply.github.com>
2020-07-19 11:07:54 +0530

    innerve 2020 website files

---
+++ js/index.js
@@ -0,0 +1,71 @@
+$(".cta1").on("mouseover",function(){
+	$(".cta1").addClass("effects");
+});
+
+
+$("#carousel").on("mouseover",function(){
+	$("#carousel .gallery-heading").addClass("effects");
+});
+
+$(".event-main-title-1").on("mouseover",function(){
+	$(".event-main-title-1").addClass("effects-2");
+});
+
+
+$(".event-main-title-2").on("mouseover",function(){
+	$(".event-main-title-2").addClass("effects-2");
+});
+
+$("#events").on("mouseover",function(){
+	$(".event-heading").addClass("effects-3");
+});
+
+
+$(".1").on("mouseover",function(){
+	$(".1 .eimage2").addClass("effect-5");
+});
+
+$(".2").on("mouseover",function(){
+	$(".2 .eimage1").addClass("effect-4");
+});
+
+
+$(".3").on("mouseover",function(){
+	$(".3 .eimage2").addClass("effect-5");
+});
+
+
+$(".4").on("mouseover",function(){
+	$(".4 .eimage1").addClass("effect-4");
+});
+
+
+$(".5").on("mouseover",function(){
+	$(".5 .eimage2").addClass("effect-5");
+});
+
+
+$(".6").on("mouseover",function(){
+	$(".6 .eimage1").addClass("effect-4");
+});
+
+
+$(".7").on("mouseover",function(){
+	$(".7 .eimage2").addClass("effect-5");
+});
+
+
+
+$(".8").on("mouseover",function(){
+	$(".8 .eimage1").addClass("effect-4");
+});
+
+
+$(".9").on("mouseover",function(){
+	$(".9 .eimage2").addClass("effect-5");
+});
+
+
+$(".10").on("mouseover",function(){
+	$(".10 .eimage1").addClass("effect-4");
+});
